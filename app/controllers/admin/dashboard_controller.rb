3e4924236f5d14d2243970d6ed0307ab82477849
samjanderson <sam.andersonnn@live.ca>
2021-03-02 23:05:10 +0000

    added admin security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+  # http_basic_authenticate_with name: "Jungle", password: "book"
   def show
   end
 end
+
+
