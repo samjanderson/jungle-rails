90752a1c515cf1dcae4347b31a44e2051dfdae58
samjanderson <sam.andersonnn@live.ca>
2021-03-02 23:55:19 +0000

    complete admin dashboard with counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   # http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @product_count = Product.count
+    @category_count = Product.group(:category_id).count
   end
 end
 
