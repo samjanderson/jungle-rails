5f0ab0c845d968bff7fbfd4cc120d2760edf4974
samjanderson <sam.andersonnn@live.ca>
2021-03-08 20:15:54 +0000

    model spec product complete

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'hoodie', description: 'test', image: 'test', price_cents: 10000, quantity: 10)
+      expect(@product).to be_valid
+      @product.errors.full_messages
+    end
+    it "is not valid without a name present" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: nil, description: 'test', image: 'test', price_cents: 10000, quantity: 10)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+    it "is not valid without a price present" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'hoodie', description: 'test', image: 'test', price_cents: nil, quantity: 10)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+    it "is not valid without a quantity present" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'hoodie', description: 'test', image: 'test', price_cents: 10000, quantity: nil)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+    it "is not valid without a category present" do
+      @product = Product.create(name: 'hoodie', description: 'test', image: 'test', price_cents: 10000, quantity: 10)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+  end
+end
