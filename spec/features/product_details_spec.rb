c1905007b41acd5ec0bbf4b9f566ffb319fccc63
samjanderson <sam.andersonnn@live.ca>
2021-03-09 04:35:22 +0000

    complete feature spec product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor goes from home page to product page by clicking on a product", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+  
+    scenario "They see details for a single product" do
+      # ACT
+      visit root_path
+      # first('article.product').click
+      #page.all('article.product')[1].click
+      # page.find(:link, 'Details', href: '/products/9').click
+      first('article.product').find_link('Details').click
+      puts page.html
+      # DEBUG
+  
+      # VERIFY
+      expect(page).to have_css '.products-show'
+      save_screenshot
+      
+    end
+
+end
